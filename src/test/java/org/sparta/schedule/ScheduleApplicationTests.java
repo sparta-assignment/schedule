d54a9b5b122378d6ea18e75b5477fe4395590ccc
hyun <hyun1202a@gmail.com>
2024-05-15 08:59:05 +0900

    :tada: first

---
+++ src/test/java/org/sparta/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
